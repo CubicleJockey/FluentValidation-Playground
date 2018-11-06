46b230be34449eb9721a3213afe55d1a65890175
André Davis <cubicle.jockey@live.com>
2018-11-06 10:18:59 -0800

    Checkpoint: Time for a meeting.

---
+++ FluentValidationPlayground/FluentValidationPlayground/Playground.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using FluentValidationPlayground.Source.Entities;
+using FluentValidationPlayground.Source.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FluentValidationPlayground
+{
+    [TestClass]
+    public class Playground
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            var customer = new Customer();
+            var validator = new CustomerValidator();
+
+            var result = validator.Validate(customer);
+
+            result.Should().NotBeNull();
+
+            if (!result.IsValid)
+            {
+                foreach (var failure in result.Errors)
+                {
+                    Console.WriteLine($"Property [{failure.PropertyName}] failed validation. Error was: {failure.ErrorMessage}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GetAllErrorMessages()
+        {
+            var customer = new Customer();
+            var validator = new CustomerValidator();
+
+            var result = validator.Validate(customer);
+
+            result.Should().NotBeNull();
+
+            var allMessages = result.ToString();
+
+            Console.WriteLine(allMessages);
+        }
+
+        [TestMethod]
+        public void GetAllErrorMessagesWithASeparator()
+        {
+            var customer = new Customer();
+            var validator = new CustomerValidator();
+
+            var result = validator.Validate(customer);
+
+            result.Should().NotBeNull();
+
+            var allMessages = result.ToString("~");
+
+            Console.WriteLine(allMessages);
+        }
+    }
+}
---
+++ FluentValidationPlayground/FluentValidationPlayground/Source/Validators/CustomerValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FluentValidationPlayground.Source.Entities;
+
+namespace FluentValidationPlayground.Source.Validators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(customer => customer.Surname).NotNull();
+        }
+    }
+}
